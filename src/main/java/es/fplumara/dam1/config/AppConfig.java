cc1787c00c11437a5cd6ad2eddea63000415f035
frc16 <153406170+frc16@users.noreply.github.com>
2026-02-12 12:48:57 +0100

    actualizaion de tarea reescrita 12/02/2026 hora 12:48

--- src/main/java/es/fplumara/dam1/config/AppConfig.java
+++ src/main/java/es/fplumara/dam1/config/AppConfig.java
@@ -1,4 +1,57 @@
 package es.fplumara.dam1.config;
 
+import es.fplumara.dam1.exceptions.ConfigException;
+import es.fplumara.dam1.files.StoreType;
+import es.fplumara.dam1.model.Message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.*;
+import java.nio.file.*;
+import java.util.Properties;
+
 public class AppConfig {
+    // atributos de la configuracion
+
+    private StoreType storeType;
+    private String messagesFile;  //
+    private int maxlength;  //tamaño del  texto
+
+    // constructor
+    public AppConfig() throws ConfigException{
+
+        Path path = Path.of("data/config.properties");// lee el archivo fuera de java/src/...
+
+        Properties props = new Properties();   // crea una instancia u objeto
+
+        try (InputStream in = Files.newInputStream(path)) {
+
+            props.load(in);
+        } catch (IOException e){
+            throw new ConfigException("no se encontro el archivo config.properties");
+            //si arriba declaro 'throw ConfigException', es necesario?
+        }
+        // falta más condicionales... para reconocer y valdidar
+
+
+    }
+    // metodos de AppConfig
+
+    public StoreType getStoreType(){ // devuelve el tipo de almacenamiento
+
+        return storeType;
+    }
+
+    public String getMessagesFile(){
+
+        return messagesFile;
+    }
+
+    public int getMaxlength(){
+
+        return maxlength;
+    }
+
 }
--- src/main/java/es/fplumara/dam1/files/TextStore.java
+++ src/main/java/es/fplumara/dam1/files/TextStore.java
@@ -1,4 +1,16 @@
 package es.fplumara.dam1.files;
 
-public interface TextStore {
+import es.fplumara.dam1.model.Message;
+
+public interface TextStore { //en las interfaces te dice que metodo se deben o pueden usar, pero no se implementa
+
+    //metodo para guardar mensaje
+    public void save(Message message);
+
+    //metodo para leer todo el texto
+    public String readAll();
+
+    //metodo para leer las ultimas n lineas
+    public String readLast(int n);
+
 }
